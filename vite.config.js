4f6a2930b3546b4d0832669d75de0dbfa986b5ab
Florakuo1112 <tingyu1112@gmail.com>
2025-02-10 02:29:59 +0800

    修改NODE_ENV設定

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:process.env.NODE_ENV === 'production'? '/react-project/' : '/',
+  base:process.env.NODE_ENV === 'production'? '/Image-Search-Application/' : '/',
   plugins: [react()],
 })
